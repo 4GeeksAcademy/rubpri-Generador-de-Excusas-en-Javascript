dc5b34c20e0b30defae49eecbcc389ad67f5ab9c
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-06-12 18:18:04 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let newwho = who[Math.floor(Math.random() * who.length)];
+  let newaction = action[Math.floor(Math.random() * action.length)];
+  let newwhat = what[Math.floor(Math.random() * what.length)];
+  let newwhen = when[Math.floor(Math.random() * when.length)];
+
+  let final = newwho + newaction + newwhat + newwhen;
+
+  let finalparahtml = document.querySelector("#excuse");
+  finalparahtml.innerHTML = final;
 };
